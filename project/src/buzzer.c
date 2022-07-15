8e54aaced6f1e3d3e6fd38a8cb351fe22b6ebe39
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-15 13:31:58 -0600

    Update buzzer.c

---
+++ project/src/buzzer.c
@@ -0,0 +1,34 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "buzzer.h"
+#include "led.h"
+
+void buzzer_init()
+{
+    timerAUpmode();
+    P2SEL2 &= ~(BIT6 | BIT7);
+    P2SEL &= ~BIT7; 
+    P2SEL |= BIT6;
+    P2DIR = BIT6;		
+}
+
+void buzzer_set_period(short cycles) 
+{
+  CCR0 = cycles; 
+  CCR1 = cycles >> 1;		
+}
+
+void buzzerOff()
+{
+  buzzer_set_period(0);
+}
+
+void song1()
+{
+
+}
+
+void song2()
+{
+
+}
