4854b8086081e6178607ebc5499f4592270a4fed
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-15 13:53:17 -0600

    Update switches.c

--- project/src/switches.c
+++ project/src/switches.c
@@ -11,17 +11,17 @@ void switch_init()
 }
 
 // state variables representing which switch was pressed down last
-int sw1 = 0; 
+int sw0 = 0; 
+int sw1 = 0;
 int sw2 = 0;
 int sw3 = 0;
-int sw4 = 0;
 
 void switch_interrupt_handler() // determine which switch was pressed down
 {
   char p2val = P2IN;
   P2IES |= (p2val & SWITCHES); // if switch up, sense down
+  sw0 = (p2val & SW0) ? 0 : 1; // if button0 up == 0 otherwise button down == 1
   sw1 = (p2val & SW1) ? 0 : 1; // if button1 up == 0 otherwise button down == 1
   sw2 = (p2val & SW2) ? 0 : 1; // if button2 up == 0 otherwise button down == 1
   sw3 = (p2val & SW3) ? 0 : 1; // if button3 up == 0 otherwise button down == 1
-  sw4 = (p2val & SW4) ? 0 : 1; // if button4 up == 0 otherwise button down == 1
 }
