d363c381a6563262c23c2bba41099c96e85d3061
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-15 13:51:22 -0600

    Update switches.h

--- project/src/switches.h
+++ project/src/switches.h
@@ -5,9 +5,9 @@
 #define SW2 BIT1
 #define SW3 BIT2
 #define SW4 BIT3
-#define SWITCHES (SW1 | SW2 | SW3 | SW4)
+#define SWITCHES (SW0 | SW1 | SW2 | SW3)
 
-extern int sw1, sw2, sw3, sw4;
+extern int sw0, sw1, sw2, sw3;
 
 void switch_init();
 void switch_interrupt_handler();
