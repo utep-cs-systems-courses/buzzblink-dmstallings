cd55a5ab5b72ecbb086bd912803a29e3b58ad904
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-17 15:42:45 -0600

    Update switches.c

--- project/src/switches.c
+++ project/src/switches.c
@@ -1,27 +1,55 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
+#include "stateMachines.h"
+#include "buzzer.h"
+#include "song.h"
 
-void switch_init()
+char switch_state_down, switch_state_changed; /* effectively boolean */
+
+static char 
+switch_update_interrupt_sense()
 {
+  char p2val = P2IN;
+  /* update switch interrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
+}
+
+void 
+switch_init()			/* setup switch */
+{  
   P2REN |= SWITCHES;		/* enables resistors for switches */
   P2IE |= SWITCHES;		/* enable interrupts from switches */
   P2OUT |= SWITCHES;		/* pull-ups for switches */
   P2DIR &= ~SWITCHES;		/* set switches' bits for input */
+  switch_update_interrupt_sense();
+  led_update();
 }
 
-// state variables representing which switch was pressed down last
-int sw0 = 0; 
-int sw1 = 0;
-int sw2 = 0;
-int sw3 = 0;
-
-void switch_interrupt_handler() // determine which switch was pressed down
+void
+switch_interrupt_handler()
 {
-  char p2val = P2IN;
-  P2IES |= (p2val & SWITCHES); // if switch up, sense down
-  sw0 = (p2val & SW0) ? 0 : 1; // if button0 up == 0 otherwise button down == 1
-  sw1 = (p2val & SW1) ? 0 : 1; // if button1 up == 0 otherwise button down == 1
-  sw2 = (p2val & SW2) ? 0 : 1; // if button2 up == 0 otherwise button down == 1
-  sw3 = (p2val & SW3) ? 0 : 1; // if button3 up == 0 otherwise button down == 1
+  char p2val = switch_update_interrupt_sense();
+  if(~p2val & SW1) {
+    switch_state_num = 1;
+  } 
+  
+  if(~p2val & SW2) {
+    switch_state_num = 2;
+  }
+
+  if(~p2val & SW3) {
+    switch_state_num = 3;
+  }
+
+  if(~p2val & SW4) {
+    switch_state_num = 4;
+  }
+  
+  if(~p2val & SWITCHES) {
+    quarter_second_count = 0;
+    songState = 0;
+  }
 }
