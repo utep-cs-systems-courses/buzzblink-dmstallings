c76caa9cc9904e85d1e1f678a5328e307371e897
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-17 15:48:24 -0600

    Create stateMachines.h

---
+++ project/src/stateMachines.h
@@ -0,0 +1,18 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+int quarter_second_count;
+//0-4
+char switch_state_num;
+
+void state_advance();
+
+void run_state();
+
+void run1();
+void run2();
+void run3();
+void run4();
+
+
+#endif // included
