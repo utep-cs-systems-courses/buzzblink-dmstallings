c850325bb089d5a183232b59a1a68b0c3cf213e2
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-17 16:23:42 -0600

    Update switches.h

--- project/toy/switches.h
+++ project/toy/switches.h
@@ -1,8 +1,11 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3		/* switch1 is p1.3 */
-#define SWITCHES SW1		/* only 1 switch on this board */
+#define SW0 BIT0		/* switch0 is p2.0 */
+#define SW1 BIT1    /* switch1 is p2.1 */
+#define SW2 BIT2    /* switch2 is p2.2 */
+#define SW3 BIT3    /* switch3 is p2.3 */
+#define SWITCHES (SW0 | SW1 | SW2 | SW3)
 
 void switch_init();
 void switch_interrupt_handler();
