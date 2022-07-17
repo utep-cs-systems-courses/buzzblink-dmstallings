a02116e3259c1bf5b9f0320183c1df8a9c997951
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-17 15:47:17 -0600

    Create song.h

---
+++ project/src/song.h
@@ -0,0 +1,15 @@
+#ifndef songStateMachine_header
+#define songStateMachine_header
+
+//State variable to track place in song
+int songState;
+
+void playSong1(void);
+
+void playSong2(void);
+
+void playSong3(void);
+
+void playSong4(void);
+
+#endif
