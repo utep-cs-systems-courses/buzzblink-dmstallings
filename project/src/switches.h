82eb647e1c1b7994faa8fcdda8ed22ca9b26012a
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-17 15:42:04 -0600

    Update switches.h

--- project/src/switches.h
+++ project/src/switches.h
@@ -1,15 +1,16 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT0
-#define SW2 BIT1
-#define SW3 BIT2
-#define SW4 BIT3
-#define SWITCHES (SW0 | SW1 | SW2 | SW3)
+#define SW1 BIT0                /* switch1 is p2.0 */
+#define SW2 BIT1                /* switch2 is p2.1 */
+#define SW3 BIT2                /* switch3 is p2.2 */
+#define SW4 BIT3                /* switch4 is p2.3 */
 
-extern int sw0, sw1, sw2, sw3;
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)  /* four switches on this board */
 
 void switch_init();
 void switch_interrupt_handler();
 
-#endif
+extern char switch_state_down, switch_state_changed; /* effectively boolean */
+
+#endif // included
