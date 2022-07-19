cc803df4389fa3bc0cf0de861226645104afefb5
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-18 20:20:56 -0600

    Update switches.h

--- project/buzzblink/switches.h
+++ project/buzzblink/switches.h
@@ -1,1 +1,17 @@
+#ifndef switches_included
+#define switches_included
 
+#define SW0 BIT0		/* switch0 is p2.0 */
+#define SW1 BIT1		/* switch1 is p2.1 */
+#define SW2 BIT2		/* switch2 is p2.3 */
+#define SW3 BIT3		/* switch3 is p2/4 */
+
+#define SWITCHES (SW0 | SW1 | SW2 | SW3) /* four switches on board */
+
+void switch_init();
+void switch_interrupt_handler();
+
+// for interrupt
+extern char switch_state_down, switch_state_changed;
+
+#endif // included
