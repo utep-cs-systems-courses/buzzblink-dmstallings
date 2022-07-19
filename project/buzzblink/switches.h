d53cf2d726633166ac1afa5101e05195e8ff2ec0
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-18 20:31:59 -0600

    Update switches.h

--- project/buzzblink/switches.h
+++ project/buzzblink/switches.h
@@ -3,8 +3,8 @@
 
 #define SW0 BIT0		/* switch0 is p2.0 */
 #define SW1 BIT1		/* switch1 is p2.1 */
-#define SW2 BIT2		/* switch2 is p2.3 */
-#define SW3 BIT3		/* switch3 is p2/4 */
+#define SW2 BIT2		/* switch2 is p2.2 */
+#define SW3 BIT3		/* switch3 is p2.3 */
 
 #define SWITCHES (SW0 | SW1 | SW2 | SW3) /* four switches on board */
 
