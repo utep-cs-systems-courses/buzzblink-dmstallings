a37b954f1686c5be58d7a828a19d543274368b2a
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-18 20:19:27 -0600

    Update main.c

--- project/buzzblink/main.c
+++ project/buzzblink/main.c
@@ -1,1 +1,15 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "switches.h"
+#include "buzzer.h"
 
+int main(void) {
+  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
+  switch_init();
+  led_init();
+  enableWDTInterrupts();	/* enable periodic interrupt */
+  buzzer_init();
+
+  or_sr(0x18);		/* CPU off, GIE on */
+}
