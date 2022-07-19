dd7b404eb78b22145fc2de2febd33fbb014dbb4c
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-18 20:34:48 -0600

    Update led.c

--- project/buzzblink/led.c
+++ project/buzzblink/led.c
@@ -1,1 +1,10 @@
+#include <msp430.h>
+#include "led.h"
+#include "switches.h"
 
+void led_init()
+{
+  P1DIR |= LEDS;		// bits attached to leds are output
+  switch_state_changed = 1;
+  led_update();
+}
