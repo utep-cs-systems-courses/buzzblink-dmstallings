7ea0444179e196d9646c634abe7c5c543a372258
dmstallings <70167904+dmstallings@users.noreply.github.com>
2022-07-20 20:54:34 -0600

    Update switches.h

--- project/buzzblink/switches.h
+++ project/buzzblink/switches.h
@@ -1,12 +1,12 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW0 BIT0		/* switch0 is p2.0 */
-#define SW1 BIT1		/* switch1 is p2.1 */
-#define SW2 BIT2		/* switch2 is p2.2 */
-#define SW3 BIT3		/* switch3 is p2.3 */
+#define SW1 BIT0		/* switch1 is p2.0 */
+#define SW2 BIT1		/* switch2 is p2.1 */
+#define SW3 BIT2		/* switch3 is p2.2 */
+#define SW4 BIT3		/* switch4 is p2.3 */
 
-#define SWITCHES (SW0 | SW1 | SW2 | SW3) /* four switches on board */
+#define SWITCHES (SW1 | SW2 | SW3 | SW4) /* four switches on board */
 
 extern char state;
 
